fc80f6699920623c76b5dd51a15fd5dc5514050f
mat285 <mat285@cornell.edu>
2024-07-13 13:40:22 -0400

    fix

--- cmd/splendor-term/websockets/main.go
+++ cmd/splendor-term/websockets/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/mat285/boardgames/games/splendor/pkg/client/terminal"
 	client "github.com/mat285/boardgames/pkg/client/http/v1alpha1"
@@ -12,11 +16,24 @@ func main() {
 
 	ctx := context.Background()
 
+	scheme := "http"
+	host := "localhost"
+	port := uint16(8080)
+
+	if len(os.Args) > 1 {
+		addr, err := Parse(os.Args[1])
+		if err == nil {
+			scheme = addr.Protocol
+			host = addr.Host
+			port = addr.Port
+		}
+	}
+
 	cli := client.New(
 		client.OptConfig(client.Config{
-			Scheme: "http",
-			Host:   "localhost",
-			Port:   8080,
+			Scheme: scheme,
+			Host:   host,
+			Port:   port,
 		}),
 		client.OptUsername("michael"),
 	)
@@ -25,4 +42,29 @@ func main() {
 	if err := term.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Address struct {
+	Protocol string
+	Host     string
+	Port     uint16
+}
+
+func Parse(addr string) (Address, error) {
+	addr = strings.TrimSuffix(addr, "/")
+	scheme := "http"
+	if strings.HasPrefix(addr, "https") {
+		scheme = "https"
+	}
+	addr = strings.TrimPrefix(addr, scheme+"://")
+	parts := strings.Split(addr, ":")
+	if len(parts) != 2 {
+		return Address{}, fmt.Errorf("invalid address")
+	}
+	host := parts[0]
+	port, err := strconv.ParseUint(parts[1], 10, 16)
+	if err != nil {
+		return Address{}, err
+	}
+	return Address{Protocol: scheme, Host: host, Port: uint16(port)}, nil
 }
